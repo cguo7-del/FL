cf6f9d66ec500da63de67e64625fba458974818f
Yuzhi Guo <yuzhiguo@Claires-MacBook-Air.local>
2025-08-27 03:15:28 -0700

    Add Monetag meta tag for monetization

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta name="monetag" content="4468a7d9ab5ccf1aac91e6acd7e06f12" />
         <script 
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5500005062081722"
